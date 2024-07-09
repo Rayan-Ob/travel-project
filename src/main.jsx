b9765de850321a7b5f3fc122a588110854550394
Rayan Obeid <rayan.obeid93@gmail.com>
2024-07-09 19:49:24 +0300

    gh-pages

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/travel-project'>
     <App />
     </BrowserRouter>
   </React.StrictMode>
